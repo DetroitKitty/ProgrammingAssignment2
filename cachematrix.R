44d262512537b1b86ffcf326f27e86a9ed262f73
DetroitKitty <Cathy@Cathys-MacBook-Pro.local>
2014-12-15 09:47:26 -0800

    HW 2
    Functions to compute and cache matrix inverse

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix and cacheSolve compute and locally store the inverse of a matrix
+## Efficient use is a single call to makeCacheMatrix
+##        mlist <- makeCacheMatrix(m)
+## followed by a call to cacheSolve whenever the inverse matrix is needed
+##        cacheSolve(mlist) 
+## 
 
-## Write a short comment describing this function
+## The input to makeCacheMatrix is the matrix to be inverted.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    minverse <- NULL
+    set <- function(y) {
+        m <<- y
+        minverse <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(minv) minverse <<- minv
+    getinverse <- function() minverse
+    invisible(list(set=set, get=get, setinverse=setinverse,getinverse=getinverse))
 }
 
 
-## Write a short comment describing this function
+## cacheSolve will return cached inverse matrix if it exists, 
+##  otherwise computes, stores to cache and returns inverse 
+## The input to cacheSolve is the list created by makeCacheMatrix
+##
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(invlist, ...) {
         ## Return a matrix that is the inverse of 'x'
+    minverse <- invlist$getinverse()
+    if (!is.null(minverse)) {
+        message("Getting cached inverse matrix")
+        return(minverse)
+    }
+    message("computing matrix inverse")
+    data <- invlist$get()
+    minverse <- solve(data)
+    invlist$setinverse(minverse)
+    invisible(minverse)
 }
